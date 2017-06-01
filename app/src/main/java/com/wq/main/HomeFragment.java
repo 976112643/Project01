2d5f598a7fd6bdc702d642968ff17879353789b5
淡定的程序员 <976112643@qq.com>
2017-06-02 00:32:37 +0800

    首页布局创建,内容还没搭建

--- app/src/main/java/com/wq/main/HomeFragment.java
+++ app/src/main/java/com/wq/main/HomeFragment.java
@@ -1,15 +1,27 @@
 package com.wq.main;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
 import com.wq.base.LBaseFragment;
 import com.wq.project01.R;
 
 /**
+ * 首页
  * Created by WQ on 2017/6/1.
  */
 
 public class HomeFragment extends LBaseFragment {
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //页面创建
+    }
+
     @Override
     public int getLayoutRes() {
-        return R.layout.activity_main;
+        return R.layout.fragment_home;
     }
 }
